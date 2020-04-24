172ce1cc0f7803d3e6632dcab0c32a5752b24667
nickfw86 <nickwatson@nicks-mbp.lan>
2020-04-24 16:50:39 -0400

    Added mapping from Color name to Color Code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //NEXT STEPS
 // 1. have npm start run the index file and drive upload. May need to use asyn code to have drive Upload wait on index to finish.
-// 2.
+// 2. Put the item, color, and size values of the Vertx Prod into their respective skus and write that to the csv so it does not have to be concatenated in sheets.
 
 
 
@@ -9,8 +9,9 @@
 // Save in the Vertx-Inventory directory
 // Run [$ csvtojson "csv_file_path" > inventory.json recommend deleteing previous inventory.json before running csvtojson
 // node index
+//node drive
 //move available_inventory.csv to https://drive.google.com/drive/folders/1-NBSKG91ZPvN7I6e-kmJC1HKTXsbiPus
-
+const colorMap = require('./colorMap')
 const productIds = require('./productIdsList');
 const fs = require('fs');
 let writeCsv = require('./createCsv')
@@ -23,6 +24,9 @@ for (let i =0; i < inventory.length; i++){
 if (productIds.indexOf(inventory[i].Itemid) > -1 ){
     vertxProd.push(inventory[i]);
 }
+}
+for (let i = 0; i < vertxProd.length; i++){
+vertxProd[i].Color = colorMap[vertxProd[i].Color];
 }
 writeCsv(vertxProd);
 
